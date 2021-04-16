796131cc2c001e1f193b26187a5d73866586fcda
Fred Grott <fred.grott@gmail.com>
2021-04-16 16:18:40 -0500

    first commit
    first commit

---
+++ lib/app/modules/appsplash/views/app_splash.dart
@@ -0,0 +1,88 @@
+// Copyright 2021 Fredrick Allan Grott. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
+import 'package:base_appbar/app/modules/homepage/views/home_page.dart';
+import 'package:base_appbar/app/themes/my_cupertino_color_scheme.dart';
+import 'package:base_appbar/app/themes/my_cupertino_text_theme.dart';
+import 'package:catcher/catcher.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class AppSplashExp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => BaseApp();
+}
+
+class BaseApp extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<BaseApp> {
+  Brightness brightness = Brightness.light;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+        data:
+            brightness == Brightness.light ? ThemeData.from(colorScheme: const ColorScheme.light()) : ThemeData.from(colorScheme: const ColorScheme.dark()),
+        child: PlatformProvider(
+          //no do not make const
+          // ignore: prefer_const_constructors
+          builder: (context) => PlatformApp(
+                // getx settings go here
+
+                debugShowCheckedModeBanner: false,
+                navigatorKey: Catcher.navigatorKey,
+
+                // ignore: prefer_const_literals_to_create_immutables
+                localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+                  DefaultMaterialLocalizations.delegate,
+                  DefaultWidgetsLocalizations.delegate,
+                  DefaultCupertinoLocalizations.delegate,
+                ],
+                title: 'Base River',
+                material: (_, __) {
+                  return MaterialAppData(
+                    // per this doc page:
+                    // https://api.flutter.dev/flutter/material/ThemeData/ThemeData.from.html
+                    // need to use this form of setting material themes as 
+                    // colors of widget components are derived form the colorscheme
+                    theme: ThemeData.from(colorScheme: const ColorScheme.light()),
+                    darkTheme: ThemeData.from(colorScheme: const ColorScheme.dark()),
+                    themeMode: brightness == Brightness.light
+                        ? ThemeMode.light
+                        : ThemeMode.dark,
+                  );
+                },
+                cupertino: (_, __) => CupertinoAppData(
+                  theme: CupertinoThemeData(
+                      brightness: brightness, // if null will use the system theme
+                      // ignore: prefer_const_constructors
+                      primaryColor: myCupertinoPrimaryColor,
+                      primaryContrastingColor:
+                          myCupertinoPrimaryContrastingColor,
+                      textTheme: myCupertinoTextThemeData,
+                      barBackgroundColor: Colors.transparent),
+                ),
+                //home: SplashScreen.navigate(
+                  //name: '2-2-tree.riv',
+                  //next: (context) => const MyHomePage(),
+                 // until: () =>
+                   //   Future<dynamic>.delayed(const Duration(seconds: 5)),
+                  //startAnimation: 'tree',
+                //),
+                home:  MyHomePage(),
+                //initialPlatform: TargetPlatform.iOS,
+              )),
+        );
+  }
+
+
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'dart:async';
+
+
+import 'package:base_appbar/app/modules/appsplash/views/app_splash.dart';
+import 'package:base_appbar/app/shared/build_modes.dart';
+import 'package:base_appbar/app/shared/init_log.dart';
+import 'package:base_appbar/app/shared/log_exception.dart';
+import 'package:base_appbar/app/shared/log_pens.dart';
+import 'package:base_appbar/app/shared/logger_types.dart';
+import 'package:catcher/catcher.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    initLog();
+    
+  } catch (error) {
+    LogException("an error: $error");
+  }
+
+  // to enable sentry add this [SentryHandler(SentryClient("YOUR_DSN_HERE"))]
+  // due to web as a target platform we do not set the snapshot path
+  // setting for catcher.
+  // Using report mode as I have found it's better feedback in getting 
+  // user to send report if the stack trace is shown to them
+  final ReportMode reportMode = PageReportMode(showStackTrace: false);
+  final CatcherOptions debugOptions =
+      // ignore: avoid_redundant_argument_values
+      CatcherOptions(reportMode, [
+    ConsoleHandler(
+        // ignore: avoid_redundant_argument_values
+        enableApplicationParameters: true,
+        // ignore: avoid_redundant_argument_values
+        enableDeviceParameters: true,
+        enableCustomParameters: true,
+        // ignore: avoid_redundant_argument_values
+        enableStackTrace: true)
+  ]);
+
+  final CatcherOptions releaseOptions = CatcherOptions(DialogReportMode(), [
+    EmailManualHandler(["email1@email.com", "email2@email.com"],
+        // ignore: avoid_redundant_argument_values
+        enableDeviceParameters: true,
+        // ignore: avoid_redundant_argument_values
+        enableStackTrace: true,
+        // ignore: avoid_redundant_argument_values
+        enableCustomParameters: true,
+        // ignore: avoid_redundant_argument_values
+        enableApplicationParameters: true,
+        // ignore: avoid_redundant_argument_values
+        sendHtml: true,
+        emailTitle: "Sample Title",
+        emailHeader: "Sample Header",
+        printLogs: true)
+  ]);
+
+  //logger.info("init completed");
+  logAFunction("main in main.dart").info(penInfo(" main init completed"));
+
+  // replaced with catcher plugin error widget in my_app.dart
+  //ErrorWidget.builder = (FlutterErrorDetails details) {
+  //if (isInDebugMode) {
+  //return ErrorWidget(details.exception);
+  // }
+
+  //return Container(
+  // alignment: Alignment.center,
+  //  child: const Text(
+  //   'Error!',
+  //   style: TextStyle(color: Colors.yellow),
+  //   textDirection: TextDirection.ltr,
+  // ),
+  //);
+  //};
+
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to
+      // app exceptions provider. We do not need this in Profile mode.
+      if (isInReleaseMode) {
+        Zone.current.handleUncaughtError(details.exception, details.stack);
+      }
+    }
+  };
+
+  runZonedGuarded<Future<void>>(
+    () async {
+      //runApp(MyApp());
+      // via the catcher plugin
+      Catcher(
+          runAppFunction: () {
+            runApp(
+              ProviderScope(child: AppSplashExp()),
+            );
+          },
+          debugConfig: debugOptions,
+          releaseConfig: releaseOptions);
+    },
+    (error, stackTrace) async {
+      await _reportError(error, stackTrace);
+    },
+    // yes we can redefine the zoneSpecification to intercept the print
+    // calls and funnel them to log calls via the logger of simple logger
+    zoneSpecification: ZoneSpecification(
+      // Intercept all print calls
+      print: (self, parent, zone, line) async {
+        // Include a timestamp and the name of the App
+        final messageToLog = "[${DateTime.now()}] Base_Riverpod $line $zone";
+
+        // Also print the message in the "Debug Console"
+        // but it's ony an info message and contains no
+        // privacy prohibited stuff
+        parent.print(zone, penInfo(messageToLog));
+      },
+    ),
+  );
+}
+
+Future<void> _reportError(dynamic error, dynamic stackTrace) async {
+  logger.info(
+    'Caught error: $error',
+  );
+  // Errors thrown in development mode are unlikely to be interesting. You
+  // check if you are running in dev mode using an assertion and omit send
+  // the report.
+  if (isInDebugMode) {
+    logger.info(
+      '$stackTrace',
+    );
+    logger
+        .info('In dev mode. Not sending report to an app exceptions provider.');
+
+    return;
+  } else {
+    // reporting error and stacktrace to app exceptions provider code goes here
+    if (isInReleaseMode) {
+      // code goes here
+    }
+  }
+}
+
+
+
